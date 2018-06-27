fba933ac333cbd6571cbc69df1c734f1324b0e2c
homobulla <homobulla@outlook.com>
2018-06-27 20:02:50 +0800

    面试题目

---
+++ test/tx.js
@@ -0,0 +1,18 @@
+var mycar = 'my value';
+(function() {
+    console.log(mycar);
+    var mycar = 'local car';
+})()
+
+function Person(name) {
+    this.name = name;
+}
+Person.prototype.age = 20;
+Person.prototype.award = [];
+var jack = new Person('jack');
+var rose = new Person('rose');
+console.log(jack.award === rose.award,jack.age === rose.age)
+jack.age++;
+rose.award.push('ocar');
+console.log(rose.age);
+console.log(jack.award);
