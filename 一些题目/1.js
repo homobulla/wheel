26f4a542bbc3497c8e5704899a13d4d8a7177151
homobulla <homo_bulla@outlook.com>
2019-06-11 19:10:59 +0800

    proxy

--- 一些题目/1.js
+++ 一些题目/1.js
@@ -1,10 +1,10 @@
 // 核心点在于用修改原始值的方法来修改this
 Array.prototype.multiply = function() {
-    let a = this.map(x => x * x)
-    this.push(...a)
-}
+    let a = this.map(x => x * x);
+    this.push(...a);
+};
 // https://juejin.im/post/5bf5610be51d452a1353b08d?utm_source=gold_browser_extension
 
-const a = [1, 2, 3, 4, 5]
-a.multiply()
-console.log(a)
+const a = [1, 2, 3, 4, 5];
+a.multiply();
+console.log(a);
